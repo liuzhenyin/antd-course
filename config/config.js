e4eac4c0ae2a775428c2fbc18345910bf8ee0f04
yinplus <liuzhenyin@126.com>
2018-12-19 16:25:15 +0800

    布局和路由设置

--- config/config.js
+++ config/config.js
@@ -1,14 +1,31 @@
 export default {
-    singular: true,
-    plugins:[
-        ['umi-plugin-react',{
+    plugins: [
+        ['umi-plugin-react', {
             antd: true
-        }]
+        }],
     ],
-    routes: [
-        {
-            path: '/',
-            component: './HelloWorld',
-        }
-    ]
+    routes: [{
+        path: '/',
+        component: '../layout',
+        routes: [
+            {
+                path: '/',
+                component: 'HelloWorld'
+            },
+            {
+                path: 'helloworld',
+                component: 'HelloWorld'
+            },
+            {
+                path: '/dashboard',
+                routes: [
+                    { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+                    { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+                    { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+                ]
+            }
+        ]
+    }
+    ],
+    singular: true,
 };
