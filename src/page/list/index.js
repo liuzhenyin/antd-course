1dec22a55b931b58fdca04e5a7fbdcbd7fef6b19
yinplus <liuzhenyin@126.com>
2018-12-20 17:11:49 +0800

    创建表格

--- src/page/list/index.js
+++ src/page/list/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import {Table} from 'antd';
+import {connect} from 'dva';
 
-export default class List extends React.Component {
+class List extends React.Component {
+    columns = [
+        {
+            title: '名称',
+            dataIndex: 'name'
+        }, {
+            title: '描述',
+            dataIndex: 'desc'
+        }, {
+            title: '链接',
+            dataIndex: 'url'
+        }
+    ];
+
     render() {
+        const {cardsList, cardsLoading} = this.props;
         return (
-            <div>hello world</div>
+            <div>
+                <Table
+                    columns={this.columns}
+                    dataIndex={cardsList}
+                    loading={cardsLoading}
+                    rowKey="id"/>
+            </div>
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {cardsList: state.cardsList, cardsLoading: state.loading.effects['cards/queryList']};
+}
+
+export default connect(mapStateToProps)(List);
